800ef9eb4222d85fe9b9b2d2fd7826e4faf9802e
Black-Swan-ICL <Black-Swan-ICL@users.noreply.github.com>
2021-03-25 12:54:31 +0000

    edited setup file

--- setup.py
+++ setup.py
@@ -1,8 +1,25 @@
+import pathlib
 from setuptools import setup, find_packages
 
+HERE = pathlib.Path(__file__).parent
+
+README = (HERE / 'README.md').read_text()
+
 setup(
-    name="PySCMs",
-    version="0.1.0",
+    name='PySCMs',
+    version='0.1.0',
+    description='A Python package for Structural Causal Models.',
+    long_description=README,
+    long_description_content_type='text/markdown',
+    url='https://github.com/Black-Swan-ICL/PySCMs',
+    author='K. M-H',
+    author_email='kmh.pro@protonmail.com',
+    license='MIT',
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+    ],
     packages=find_packages(),
     install_requires=['numpy', 'scipy', 'pandas', 'pytest']
 )
