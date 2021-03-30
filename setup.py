5727c776abb7643a424931900f6ec56ed35eeb85
Black-Swan-ICL <Black-Swan-ICL@users.noreply.github.com>
2021-03-30 14:10:26 +0100

    Updating versioning

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ README = (HERE / 'README.md').read_text()
 
 setup(
     name='StructuralCausalModels',
-    version='1.0.1',
+    version='2.0.0',
     description='A Python package for Structural Causal Models.',
     long_description=README,
     long_description_content_type='text/markdown',
