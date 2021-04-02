63110eca6c16d79bab081e9ec5f8e6fa2e786e86
Black-Swan-ICL <Black-Swan-ICL@users.noreply.github.com>
2021-04-02 14:37:00 +0100

    Graph class now fully documented

--- StructuralCausalModels/graph.py
+++ StructuralCausalModels/graph.py
@@ -9,8 +9,34 @@ from StructuralCausalModels.graph_via_edges import EdgeType, GraphViaEdges, \
 
 
 class Graph:
+    """A class to represent graphs.
 
-    # TODO document
+    A Graph object is a representation of the graph. The graph may be very
+    general : it does not have to be directed, or acyclic for instance. It
+    is arguably easiest to think of a graph in terms of its adjacency matrix
+    which is why this implementation is 'adjacency matrix'-driven. Other
+    representations can be more convenient depending on context, which is
+    why these representations are automatically generated upon construction
+    and stored as attributes of the Graph object.
+
+    Parameters
+    ----------
+    adjacency_matrix : array_like
+        The adjacency matrix of the graph.
+    name : str, optional
+        The name of the object created (default is '').
+
+    Attributes
+    ----------
+    adjacency_matrix_representation : GraphViaAdjacencyMatrix
+        The representation of the graph based on the adjacency matrix.
+    adjacency_list_representation : GraphViaAdjacencyLists
+        The representation of the graph based on the adjacency lists.
+    edge_representation : GraphViaEdges
+        The adjacency of the graph based on the typed (undirected, forward etc.)
+        edges.
+    """
+
     def __init__(self, adjacency_matrix, name=''):
         # Representation of the graph via an adjacency matrix
         matrix_based = GraphViaAdjacencyMatrix(
@@ -36,24 +62,48 @@ class Graph:
         self.adjacency_list_representation = list_based
         self.edge_representation = edge_based
 
-    # TODO document
     @staticmethod
     def validate_binary_matrix(matrix):
+        """Validates that a matrix is a proper adjacency matrix.
+
+        A proper adjacency matrix contains only 0's and 1's.
+
+        Parameters
+        ----------
+        matrix : array_like
+            The matrix to validate.
+
+        Returns
+        -------
+        bool
+            Whether the matrix is a valid adjacency matrix.
+        """
+
         return GraphViaAdjacencyMatrix.validate_binary_matrix(matrix)
 
-    # TODO document
     @property
     def adjacency_matrix(self):
+        """array_like: the adjacency matrix of the graph."""
         return self.adjacency_matrix_representation.adjacency_matrix
 
-    # TODO document
     @property
     def name(self):
+        """str: the name of the graph."""
         return self.adjacency_matrix_representation.name
 
-    # TODO document
     @adjacency_matrix.setter
     def adjacency_matrix(self, new_adjacency_matrix):
+        """Sets adjacency matrix of a graph to a new value.
+
+        When changing the adjacency matrix of the graph, the different graph
+        representations are automatically re-generated to maintain consistency.
+
+        Parameters
+        ----------
+        new_adjacency_matrix : array_like
+            The new adjacency matrix of the graph.
+        """
+
         # Re-generate the adjacency matrix based representation of the graph
         new_matrix_based = GraphViaAdjacencyMatrix(
             adjacency_matrix=new_adjacency_matrix,
@@ -78,10 +128,36 @@ class Graph:
         self.adjacency_list_representation = new_list_based
         self.edge_representation = new_edge_based
 
-    # TODO document
     def structural_hamming_distance(self,
                                     other,
                                     penalty_edge_mismatch_func=None):
+        """Computes the Structural Hamming Distance between two graphs.
+
+        By default, the Structural Hamming Distance (SHD) is equal to the number
+        of edges in the graphs that are not of the same type. A different
+        weighted scheme for penalty computation may be provided (we may want to
+        penalise the presence of an edge in the opposite direction more than the
+        absence of an edge, for example).
+
+        Parameters
+        ----------
+        other : Graph
+            The graph to compare.
+        penalty_edge_mismatch_func : callable, optional
+            The edge mismatch penalty scheme (default is None in which case a
+            built-in function is used).
+
+        Returns
+        -------
+        float
+            The Structural Hamming Distance (SHD), in its default form or under
+            a user-provided edge mismatch penalty scheme.
+
+        Raises
+        ------
+        GraphsCannotBeCompared
+            Raised if the graphs do not have the same vertex set.
+        """
 
         res = self.edge_representation.structural_hamming_distance(
             other=other.edge_representation,
@@ -90,9 +166,20 @@ class Graph:
 
         return res
 
-    # TODO document
     @staticmethod
     def adjacency_matrix_to_adjacency_lists(adjacency_matrix):
+        """Converts an adjacency matrix to the corresponding adjacency lists.
+
+        Parameters
+        ----------
+        adjacency_matrix : array_like
+            The adjacency matrix.
+
+        Returns
+        -------
+        list
+            The adjacency lists.
+        """
 
         nb_vertices = adjacency_matrix.shape[0]
 
@@ -105,9 +192,20 @@ class Graph:
 
         return adjacency_lists
 
-    # TODO document
     @staticmethod
     def adjacency_lists_to_adjacency_matrix(adjacency_lists):
+        """Converts adjacency lists to the corresponding adjacency matrix.
+
+        Parameters
+        ----------
+        adjacency_lists : list
+            The adjacency lists.
+
+        Returns
+        -------
+        array_like
+            The adjacency matrix.
+        """
         nb_vertices = len(adjacency_lists)
         # Build the adjacency matrix
         adjacency_matrix = np.zeros((nb_vertices, nb_vertices))
@@ -117,9 +215,27 @@ class Graph:
         return adjacency_matrix
 
     # TODO test
-    # TODO document
     @staticmethod
     def compute_edge_type(m_ij, m_ji):
+        """Determines the type of the edge between X_i and X_j.
+
+        Determines the type of the edge between X_i and X_j based on the values
+        of M[i, j] and M[j, i] where M is the adjacency matrix of the graph.
+
+        Parameters
+        ----------
+        m_ij : int
+            The element in the adjacency matrix at the intersection of the i-th
+            row and the j-th column.
+        m_ji : int
+            The element in the adjacency matrix at the intersection of the j-th
+            row and the i-th column.
+
+        Returns
+        -------
+        EdgeType
+            The typed edge between X_i and X_j in the graph.
+        """
 
         if m_ij == 0 and m_ji == 0:
 
@@ -141,9 +257,24 @@ class Graph:
 
             raise ImpossibleEdgeConfiguration
 
-    # TODO document
     @staticmethod
     def adjacency_matrix_to_edges(adjacency_matrix):
+        """Converts adjacency matrix to the corresponding typed edges.
+
+        Parameters
+        ----------
+        adjacency_matrix : array_like
+            The adjacency matrix.
+
+        Returns
+        -------
+        dict
+            The typed edges.
+
+            The keys are tuples (i, j) ; they indicates the edge is between X_i
+            and X_j in the graph. The values are EdgeType objects which indicate
+            what type of edge is between X_i and X_j in the graph.
+        """
 
         edges = dict()
 
@@ -156,10 +287,25 @@ class Graph:
 
         return edges
 
-    # TODO document
     # TODO test
     @staticmethod
     def edges_to_adjacency_lists(edges):
+        """Converts the typed edges to the corresponding adjacency matrix.
+
+        Parameters
+        ----------
+        edges : dict
+            The typed edges.
+
+            The keys are tuples (i, j) ; they indicates the edge is between X_i
+            and X_j in the graph. The values are EdgeType objects which indicate
+            what type of edge is between X_i and X_j in the graph.
+
+        Returns
+        -------
+        array_like
+            The adjacency matrix.
+        """
 
         parent_children = dict()
 
