d2f7bcc87df10fba5dc856e536bf8783f2fb8ef8
Black-Swan-ICL <Black-Swan-ICL@users.noreply.github.com>
2021-04-02 10:23:49 +0100

    Added some doc, todo's + tests for the edge representation

--- StructuralCausalModels/graph_via_edges.py
+++ StructuralCausalModels/graph_via_edges.py
@@ -7,6 +7,18 @@ class EdgeType(Enum):
     BACKWARD = '<-'
     UNDIRECTED = '--'
 
+    def __repr__(self):
+        """
+        Returns a string representation of the object from which it can be
+        rebuilt.
+
+        Returns
+        -------
+        str
+            A string representation of the object from which it can be rebuilt.
+        """
+        return f"EdgeType.{self.name}"
+
 
 class GraphsCannotBeCompared(Exception):
 
@@ -21,13 +33,29 @@ class ImpossibleEdgeConfiguration(Exception):
 class GraphViaEdges:
 
     # TODO parameter validation
+    # TODO document
     def __init__(self, edges, name=''):
         self.edges = edges
         self.name = name
 
-    # TODO document
     @staticmethod
     def compute_penalty(edge_1, edge_2):
+        """
+        Provides a default method to compute the penalty incurred when two edges
+        are of a same type or of different types.
+
+        Parameters
+        ----------
+        edge_1 : EdgeType
+            The first edge.
+        edge_2 : EdgeType
+            The second edge.
+
+        Returns
+        -------
+        float
+            The penalty incurred.
+        """
 
         if edge_1 == edge_2:
             return 0
@@ -46,10 +74,34 @@ class GraphViaEdges:
         else:
             raise ImpossibleEdgeConfiguration
 
-    # TODO document
     def structural_hamming_distance(self,
                                     other,
                                     penalty_edge_mismatch_func=None):
+        """
+        Computes the Structual Hamming Distance between two graphs. By default
+        it is equal to the number of edges in the graphs that are not of the
+        same type. A different weighted scheme for penalty computation may be
+        provided (we may want to penalise the presence of an edge in the
+        opposite direction more than the absence of an edge, for example).
+
+        Parameters
+        ----------
+        other : GraphViaEdges
+            The graph to compare.
+        penalty_edge_mismatch_func : callable
+            The edge mismatch penalty scheme.
+
+        Returns
+        -------
+        float
+            The Structural Hamming Distance (SHD), in its default form or under
+            a user-provided edge mismatch penalty scheme.
+
+        Raises
+        ------
+        GraphsCannotBeCompared
+            Raised if the graphs do not have the same vertex set.
+        """
 
         edges_1 = self.edges
         edges_2 = other.edges
@@ -72,6 +124,56 @@ class GraphViaEdges:
 
         return shd
 
-    # TODO __str__ method
-    # TODO __repr__ method
-    # TODO __eq__ method
+    def __str__(self):
+        """
+        Returns a user-friendly string representation of the object.
+
+        Returns
+        -------
+        str
+            A user-friendly string representation of the object.
+        """
+        s = f"GraphViaEdges '{self.name}',\nedges :\n"
+        for edge, edgetype in self.edges.items():
+            s += f"    {edge[0]} {edgetype.value} {edge[1]}\n"
+
+        return s
+
+    def __repr__(self):
+        """
+        Returns a string representation of the object from which it can be
+        rebuilt.
+
+        Returns
+        -------
+        str
+            A string representation of the object from which it can be rebuilt.
+        """
+        s = f"GraphViaEdges(name={repr(self.name)}, "
+        s += f"edges={repr(self.edges)})"
+
+        return s
+
+    def __eq__(self, other):
+        """
+        Checks whether the object is equal to another GraphViaEdges object. Two
+        GraphViaEdges objects are equal if they have the same edges and the same
+        names.
+
+        Parameters
+        ----------
+        other : GraphViaEdges
+            The other GraphViaEdges object.
+
+        Returns
+        -------
+        bool
+            Whether the two GraphViaEdges objects are equal.
+        """
+        if self.edges != other.edges:
+            return False
+
+        if self.name != other.name:
+            return False
+
+        return True
--- StructuralCausalModels/test/test_graph_via_edges.py
+++ StructuralCausalModels/test/test_graph_via_edges.py
@@ -2,6 +2,29 @@ import pytest
 
 from StructuralCausalModels.graph_via_edges import EdgeType, GraphViaEdges
 
+
+@pytest.fixture
+def graph_via_edges_example():
+
+    example = GraphViaEdges(
+        edges={
+            (0, 0): EdgeType.NONE,
+            (0, 1): EdgeType.UNDIRECTED,
+            (0, 2): EdgeType.FORWARD,
+            (0, 3): EdgeType.FORWARD,
+            (1, 1): EdgeType.NONE,
+            (1, 2): EdgeType.FORWARD,
+            (1, 3): EdgeType.FORWARD,
+            (2, 2): EdgeType.NONE,
+            (2, 3): EdgeType.UNDIRECTED,
+            (3, 3): EdgeType.NONE
+        },
+        name=''
+    )
+
+    return example
+
+
 @pytest.mark.parametrize(
     "edge_1, edge_2, expected_penalty",
     [
@@ -14,3 +37,11 @@ def test_compute_penalty_edge_mismatch(edge_1, edge_2, expected_penalty):
     actual_penalty = GraphViaEdges.compute_penalty(edge_1, edge_2)
 
     assert actual_penalty == expected_penalty
+
+
+def test_can_rebuild_graph_from_edges(graph_via_edges_example):
+
+    reconstructed = eval(repr(graph_via_edges_example))
+
+    assert reconstructed == graph_via_edges_example
+
