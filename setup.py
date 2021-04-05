649aa646ec7f0065216b27c4b542848417e32ac6
Black-Swan-ICL <Black-Swan-ICL@users.noreply.github.com>
2021-04-05 14:59:08 +0100

    Bumped version number up + delete dummy requirements (see if that breaks nothing)

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ README = (HERE / 'README.md').read_text()
 
 setup(
     name='StructuralCausalModels',
-    version='2.0.0',
+    version='3.0.0',
     description='A Python package for Structural Causal Models.',
     long_description=README,
     long_description_content_type='text/markdown',
