d7e7ae9824a09f925246566e413e89ccea0363af
Black-Swan-ICL <Black-Swan-ICL@users.noreply.github.com>
2021-04-05 11:49:43 +0100

    Adding pytest in the autodoc mock imports

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -55,4 +55,4 @@ html_theme = 'sphinx_rtd_theme'
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
-autodoc_mock_imports = ['numpy', 'scipy', 'pandas']
+autodoc_mock_imports = ['numpy', 'scipy', 'pandas', 'pytest']
