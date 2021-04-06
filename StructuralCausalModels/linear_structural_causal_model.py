e1a0e207a6c73c5d6928de32c24bb892a7b9fe0d
Black-Swan-ICL <Black-Swan-ICL@users.noreply.github.com>
2021-04-06 13:18:35 +0100

    Documented linear scm module

--- StructuralCausalModels/linear_structural_causal_model.py
+++ StructuralCausalModels/linear_structural_causal_model.py
@@ -6,27 +6,79 @@ from StructuralCausalModels.structural_causal_model import StructuralCausalModel
 
 
 class InvalidWeightedAdjacencyMatrix(Exception):
-
+    """Raised if the weighted adjacency matrix does not define a directed graph.
+    """
     pass
 
 
 class InvalidNumberOfExogenousVariables(Exception):
+    """Raised if the number of exogenous variables provided is incorrect.
 
+    By incorrect is meant that the number of exogenous variables provided is
+    inconsistent with the number of variables expected in the SCM.
+    """
     pass
 
 
 class LinearStructuralCausalModel(StructuralCausalModel):
+    """A class to represent linear Structural Causal Models.
 
-    # TODO document
+    Beware, does not check the equation are linear.
+
+    Parameters
+    ----------
+    nb_var : int
+        The number of variables in the SCM.
+    structural_equations : list
+        The list of the structural equations defining the SCM.
+    name : str, optional
+        The name of the SCM (default is '').
+    """
+
     def __init__(self, name, nb_var, structural_equations):
         super().__init__(name=name,
                          nb_var=nb_var,
                          structural_equations=structural_equations)
 
-    # TODO document
     @staticmethod
-    def create_from_coefficient_matrix(name, matrix, causal_order,
-                                       exogenous_variables):
+    def create_from_coefficient_matrix(matrix, causal_order,
+                                       exogenous_variables, name=''):
+        """Creates a linear SCM from a coefficient matrix.
+
+        The coefficient matrix is interpreted as the weighted adjacency matrix
+        of the graph associated to a linear SCM.
+
+        Parameters
+        ----------
+        matrix : numpy.ndarray
+            The weighted adjacency matrix of the graph associated to the linear
+            SCM to build.
+        causal_order : array_like
+            A causal order of the graph associated to the SCM.
+        exogenous_variables : list
+            The list of the exogenous variables. Note that the list must be
+            ordered in the "natural" way - not necessarily in the causal
+            ordering. In other terms, exogenous_varoables[0] contains the
+            exogenous variables for the structural equation that has :math:`X_0`
+            on its left-hand side etc.
+        name : str, optional
+            The name of the linear SCM created (default is '').
+
+        Returns
+        -------
+        LinearStructuralCausalModel
+            The linear SCM corresponding to the weighted adjacency matrix
+            provided.
+
+        Raises
+        ------
+        InvalidWeightedAdjacencyMatrix
+            If the weighted adjacency matrix provided defines a graph that is
+            not a directed graph.
+        InvalidNumberOfExogenousVariables
+            If the number of exogenous variables provided does not correspond to
+            the number of variables expected from the matrix provided.
+        """
 
         # Check that the matrix is a valid weighted adjacency matrix for a
         # directed graph
