0a566f801ab72b0150d720eb0b6e94e1ad5fa368
Black-Swan-ICL <Black-Swan-ICL@users.noreply.github.com>
2021-04-06 14:34:35 +0100

    Attempting to get RTD to render numpy docstrings properly

--- setup.py
+++ setup.py
@@ -27,11 +27,10 @@ setup(
     install_requires=['numpy', 'scipy', 'pandas', 'pytest'],
     docs_extras=[
         'Sphinx >= 3.5.3',
-        "sphinx.ext.napoleon",
         'numpy >= 1.20.1',
         'scipy > 1.6.2',
         'pandas >= 1.2.3',
         'pytest >= 6.2.2',
-        'mock >= 4.0.3'
+        # 'mock >= 4.0.3'
     ]
 )
